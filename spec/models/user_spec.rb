44107f9d7c5dca7f2289b6a95889b0ec59914b1f
Kalash Tuladhar <kalashtuladhar@gmail.com>
2012-11-14 18:07:54 -0600

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-  #it { should respond_to(:remember_token) }
+  it { should respond_to(:remember_token) }
   #it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
   #it { should respond_to(:microposts) }
@@ -126,10 +126,10 @@ describe User do
     end
   end  # good
 
-  # describe "remember token" do
-  #   before { @user.save }
-  #   its(:remember_token) { should_not be_blank }
-  # end
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
   # describe "micropost associations" do
     
